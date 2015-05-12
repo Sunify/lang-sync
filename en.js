64770dc4e5953b1d65d586e4ee293280b85a61dc
Александр Лунев <isuntc@gmail.com>
2015-05-12 13:32:16 +0800

    Lang files v1

---
+++ en.js
@@ -0,0 +1,14 @@
+export default {
+	"yeap": "Yeap",
+	"error": "Error",
+	"nope": "Nope",
+	"goal": {
+		"name": "Name",
+		"options": {
+			1: "One",
+			2: "Two",
+			3: "Three"
+		}
+	},
+	"values": [1, 2, 3]
+};
--- index.js
+++ index.js
@@ -2,21 +2,6 @@ import differ from 'deep-diff';
 import normalizeDiff from './normalizeDiff';
 import applyDiff from './applyDiff';
 
-const ru1 = {
-	"yeap": "Да",
-	"error": "Ошибка",
-	"nope": "Нет",
-	"goal": {
-		"name": "Имя",
-		"options": {
-			1: "Раз",
-			2: "Два",
-			3: "Три"
-		}
-	},
-	"values": [1, 2, 3]
-};
-
 const ru2 = {
 	"yeap": "Да",
 	"error": "Ошибка",
@@ -33,24 +18,9 @@ const ru2 = {
 	"valuez": [1, 2, 3]
 };
 
-const en = {
-	"yeap": "Yeap",
-	"error": "Error",
-	"nope": "Nope",
-	"goal": {
-		"name": "Name",
-		"options": {
-			1: "One",
-			2: "Two",
-			3: "Three"
-		}
-	},
-	"values": [1, 2, 3]
-};
+// const diff = normalizeDiff(differ(ru1, ru2) || []);
+// console.log(diff);
 
-const diff = normalizeDiff(differ(ru1, ru2) || []);
-console.log(diff);
-
-applyDiff(en, diff).then((en2) => {
-	console.log(en2);
-});
+// applyDiff(en, diff).then((en2) => {
+// 	console.log(en2);
+// });
